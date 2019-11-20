10e242ce4dad327a97d77b2f52a7cbc96236266d
Priyanka Khadilkar <49756361+Priyanka-Khadilkar@users.noreply.github.com>
2019-11-20 08:33:29 -0500

    first portfolio commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "240px";
+}
+
+function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+}
